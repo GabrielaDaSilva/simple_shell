564aa6e6152867871106cfee171b4548f9aaeed4
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-05-10 00:00:11 -0700

    header and main files

---
+++ main_file.c
@@ -0,0 +1,46 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "shell_header.h"
+
+/**
+* main – Entry point
+* @argc: number of arguments
+* @argv: array of pointers to arguments
+* Return: void.
+*/
+
+int main(int argc, char **argv)
+{
+	int nread;
+	char *command;
+	const char *prompt = “our_shell>“;
+	size_t nbytes = sizeof(prompt), line_size = 1024;
+
+	if (argc > 1)
+	{
+		exit(0);
+	}
+
+	while (1)
+	{
+		command = malloc(100);
+		write(STDIN_FILENO, prompt, nbytes);
+		nread = getline(&command, &line_size, stdin);
+
+		if (nread == -1)
+		{
+			free(command);
+			return (0);
+		}
+
+		else if (nread > 1)
+		{
+			command[nread – 1] = ‘\0’;
+			if (command)
+				parse(command, environ, argv[0]);
+			free(command);
+		}
+	}
+}
---
+++ shell_header.h
@@ -0,0 +1,69 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+#define MAXARGS 128
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+/**
+* struct cmd – holds all arguments and is most important.
+* cmd: command.
+* @home: home.
+* @argc: number of args
+* @argv: array of pointers to args
+* @env: array of pointers to env vars
+* Description: structure for commands.
+*/
+
+typedef struct cmd
+{
+int argc;
+char *argv[MAXARGS];
+char **env;
+char *home;
+} cmd;
+
+/**
+* struct builtins – defines what our builtin functions
+* @instruction: instructions
+* @func: function.
+* Descriiption: structure or builtin commands.
+*/
+
+typedef struct builtins
+{
+char *instruction;
+void(*func)(cmd *);
+} builtins_table;
+
+void parse(char *command, char **env, char *home);
+void execute(struct cmd *cmd_struct, char *new_path);
+void (*is_builtin(char *command))(cmd *cmd_struct);
+void my_cd(cmd *cmd_struct);
+void my_exit(cmd *cmd_struct);
+void my_env(cmd *cmd_struct);
+void eval(cmd *cmd_struct);
+char **environ;
+
+/**
+ * String functions that we can write to assist us*
+ */
+
+int _strcmp(char *s1, char *s2);
+char *get_env_var(char **environ, char *var);
+char *check_var(char *env_var, char *var);
+char *handle_path(cmd *cmd_struct, char *checkpath);
+char *_strcat(char *dest, char *src);
+
+/**
+ * Helpers
+ */
+
+int check_file(char *file);
+size_t get_len(char *s);
+
+#endif
