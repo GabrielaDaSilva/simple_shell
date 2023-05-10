89aa5efc4b432b8285807f62cb91b0f33c81dbae
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-05-10 03:45:46 -0700

    update, files added

---
+++ evaluate_file.c
@@ -0,0 +1,24 @@
+#include "shell_header.h"
+
+/**
+* eval – Determines whether the command is builtin,
+* system or invalid and takes action.
+* @cmd_struct: command structure.
+* Return: void.
+*/
+
+void eval(cmd *cmd_struct)
+{
+	char checkpath[20] = "/bin/", *new_path;
+	void (*func)(cmd *cmd_struct) = is_builtin(cmd_struct->argv[0]);
+
+	if (func)
+	{
+		func(cmd_struct);
+	}
+	else
+	{
+		new_path = handle_path(cmd_struct, checkpath);
+		execute(cmd_struct, new_path);
+	}
+}
---
+++ execute_file.c
@@ -0,0 +1,39 @@
+#include "shell_header.h"
+
+/**
+* execute – Executes system calls
+* @cmd_struct: command structure
+* @newpath: path of file
+* Return: no return
+*/
+
+void execute(cmd *cmd_struct, char *newpath)
+{
+	pid_t child_pid;
+
+	if (check_file(newpath))
+	{
+		while ((child_pid = fork()) < 0)
+		{
+			perror("fork error");
+			exit(1);
+		}
+
+		if (child_pid == 0)
+		{
+			if (execve(newpath, cmd_struct->argv, cmd_struct->env) < 0)
+			{
+				perror(cmd_struct->home);
+				exit(1);
+			}
+		}
+		else
+		{
+			wait(&child_pid);
+		}
+	}
+	else if (execve(newpath, cmd_struct->argv, cmd_struct->env) < 0)
+	{
+		perror(cmd_struct->home);
+	}
+}
