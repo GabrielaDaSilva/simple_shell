a4a2908727f74c97bb53780283a04f7512a3b1b8
GabrielaDaSilva <111237935+GabrielaDaSilva@users.noreply.github.com>
2023-05-10 10:12:25 +0200

    Update main_file.c

--- main_file.c
+++ main_file.c
@@ -8,39 +8,59 @@
 * main – Entry point
 * @argc: number of arguments
 * @argv: array of pointers to arguments
-* Return: void.
 */
-
 int main(int argc, char **argv)
+
 {
-	int nread;
-	char *command;
-	const char *prompt = “our_shell>“;
+
+	int nread;	char *command;
+
+	const char *prompt = "myshell> ";
+
 	size_t nbytes = sizeof(prompt), line_size = 1024;
 
 	if (argc > 1)
+
 	{
+
 		exit(0);
+
 	}
 
 	while (1)
+
 	{
+
 		command = malloc(100);
+
 		write(STDIN_FILENO, prompt, nbytes);
+
 		nread = getline(&command, &line_size, stdin);
 
 		if (nread == -1)
+
 		{
+
 			free(command);
+
 			return (0);
+
 		}
 
 		else if (nread > 1)
+
 		{
-			command[nread – 1] = ‘\0’;
+
+			command[nread - 1] = '\0';
+
 			if (command)
+
 				parse(command, environ, argv[0]);
+
 			free(command);
+
 		}
+
 	}
+
 }
