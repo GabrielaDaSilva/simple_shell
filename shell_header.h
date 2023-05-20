fce24fe7292429f00046f82e26c1a7000c7535c8
GabrielaDaSilva <111237935+GabrielaDaSilva@users.noreply.github.com>
2023-05-20 16:08:00 +0200

    Update shell_header.h

--- shell_header.h
+++ shell_header.h
@@ -9,35 +9,35 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+extern char **environ;
+
 /**
  * struct cmd - holds all arguments and is most important.
+ * cmd: command.
  * @home: home.
  * @argc: number of args
  * @argv: array of pointers to args
  * @env: array of pointers to env vars
- * Description: structure for builtin commands
+ * Description: structure for commands.
  */
-
 typedef struct cmd
 {
-int argc;
-char *argv[MAXARGS];
-char **env;
-char *home;
+	int argc;
+	char *argv[MAXARGS];
+	char **env;
+	char *home;
 } cmd;
 
 /**
  * struct builtins - defines what our builtin functions
  * @instruction: instructions
  * @func: function.
- * Description: structure for a builtin command
+ * Descriiption: structure or builtin commands.
  */
-
 typedef struct builtins
 {
-char *instruction;
-void(*func)
-(cmd *);
+	char *instruction;
+	void (*func)(cmd *);
 } builtins_table;
 
 void parse(char *command, char **env, char *home);
@@ -47,13 +47,15 @@ void my_cd(cmd *cmd_struct);
 void my_exit(cmd *cmd_struct);
 void my_env(cmd *cmd_struct);
 void eval(cmd *cmd_struct);
-char **environ;
+
+/* String functions that we can write to assist us */
 int _strcmp(char *s1, char *s2);
 char *get_env_var(char **environ, char *var);
 char *check_var(char *env_var, char *var);
 char *handle_path(cmd *cmd_struct, char *checkpath);
 char *_strcat(char *dest, char *src);
+
+/* Helpers */
 int check_file(char *file);
 size_t get_len(char *s);
-
 #endif
