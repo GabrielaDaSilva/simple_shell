26b8f6aad28fdc00d7d7350a6df514cb6b5cf8ef
GabrielaDaSilva <111237935+GabrielaDaSilva@users.noreply.github.com>
2023-05-20 16:06:10 +0200

    Update parse_file.c

--- parse_file.c
+++ parse_file.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "shell_header.h"
-
 /**
  * parse - Tokenizes the string (splits words based on
  * space character) and sends to eval.
@@ -10,27 +9,33 @@
  * @env: global environment
  * @home: filename of executable
  * Return: no return
-*/
+ */
 
 void parse(char *command, char **env, char *home)
 {
-	cmd *cmd_struct = 0;
+	cmd *cmd_struct = malloc(sizeof(cmd));
 	char *token;
 	const char *delim = " ";
 
 	cmd_struct->argc = 0;
+	/* Tokenize command string */
 	token = strtok(command, delim);
-	(cmd_struct->argv)[cmd_struct->argc] = token;
-
+	/* Add token to argv list for execve */
+	(cmd_struct->argv)[cmd_struct->argc] =  token;
 	while (token != NULL)
 	{
 		cmd_struct->argc++;
 		token = strtok(NULL, delim);
-		(cmd_struct->argv)[cmd_struct->argc] = token;
+		(cmd_struct->argv)[cmd_struct->argc] =  token;
 	}
 	cmd_struct->env = env;
 	cmd_struct->home = home;
-
+	/**
+	 * Ensure Command is tokenized
+	 * while (cmd_struct->argv[count] != NULL){
+	 * printf("%s\n", cmd_struct->argv[count]);
+	 * count++;}
+	*/
 	if (cmd_struct->argc)
 	{
 		eval(cmd_struct);
